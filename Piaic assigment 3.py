6ab3dfdeb419ca221de0e8ff6101fd5a2b31a8a0
Abeerah Ateeq <abeerahateeq@gmail.com>
2024-08-19 07:11:33 -0700

    Add files via upload

---
+++ Piaic assigment 3.py
@@ -0,0 +1,44 @@
+
+# Age Calculation
+birth_year = 2007
+current_year = 2024
+age = current_year - birth_year
+print("Your age is:", age)
+
+#Area of rectangle
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+area = length * width
+print("The area of the rectangle is:", area)
+
+# Area of circle
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+area = length * width
+print("The area of the rectangle is:", area)
+
+# Area of cube
+side_length = float(input("Enter the side length of the cube: "))
+surface_area = 6 * (side_length ** 2)
+print("The surface area of the cube is:", surface_area)
+
+# Time units conversion
+total_seconds = 200
+minutes = total_seconds // 60
+seconds = total_seconds % 60
+print("Minutes:", minutes)
+print("Seconds:", seconds)
+
+# Calculating %
+numerator = float(input("Enter the part: "))
+denominator = float(input("Enter the whole: "))
+percentage = (numerator / denominator) * 100
+print("Percentage:", percentage, "%")
+# Temperature unit conversion
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+celsius = (fahrenheit - 32) * 5 / 9
+print(f"{fahrenheit:.2f}°F is equivalent to {celsius:.2f}°C")
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9 / 5) + 32
+print(f"{celsius:.2f}°C is equivalent to {fahrenheit:.2f}°F")
+
